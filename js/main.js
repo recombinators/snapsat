63e0bd20c10cda63f66f63d55cc0d88a026a85ba
jacquestardie <hi@jacquestardie.org>
2015-03-21 12:35:36 -0700

    configure gulp

---
+++ js/main.js
@@ -0,0 +1,4 @@
+L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
+var map = L.mapbox.map('map', 'jacques.map-612s2qsj');
+map.setView([47.568, -122.582], 9);
+var hash = L.hash(map);
