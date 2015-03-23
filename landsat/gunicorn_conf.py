95445fc639622f463ec9ae187d4a9dfd4fe226f6
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-23 11:44:38 -0700

    let's get gunicorn serving up pyramid

---
+++ landsat/gunicorn_conf.py
@@ -0,0 +1,14 @@
+#gunicorn_conf.py
+import os
+
+def numCPUs():
+    if not hasattr(os, "sysconf"):
+        raise RuntimeError("No sysconf detected.")
+    return os.sysconf("SC_NPROCESSORS_ONLN")
+
+workers = numCPUs() * 2 + 1
+bind = "127.0.0.1:8000"
+pidfile = "/tmp/gunicorn-app.pid"
+backlog = 2048
+logfile = "/var/log/gunicorn-app.log"
+loglevel = "info"
