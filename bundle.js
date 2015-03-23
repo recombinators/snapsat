8ceede74c4c5e4d43398afe6cd1ad970d84cbe31
jacquestardie <hi@jacquestardie.org>
2015-03-23 10:10:36 -0700

    Get browserify running with Mapbox.

---
+++ bundle.js
@@ -0,0 +1,7 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
+var map = L.mapbox.map('map', 'jacques.map-612s2qsj');
+map.setView([47.568, -122.582], 9);
+var hash = L.hash(map);
+
+},{}]},{},[1]);
