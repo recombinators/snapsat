244e361f29bd9640bf432de0f85b92b17d39880e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-24 12:51:43 -0700

    #15 change connect to sqs method

--- app/app/sqs.py
+++ app/app/sqs.py
@@ -1,13 +1,12 @@
-from boto.sqs import connect_to_region
+from boto.sqs import connect_sqs
 from boto.sqs.message import Message
 
 
 def make_connection(**kwargs):
     '''Make a connection to an AWS account. Kwargs is a dictionary of the AWS
        region, AWS access key id, and AWS secret access key'''
-    return connect_to_region(kwargs['region'],
-                             aws_access_key_id=kwargs['aws_access_key_id'],
-                             aws_secret_access_key=kwargs['aws_secret_access_key'])
+    return connect_sqs(aws_access_key_id=kwargs['aws_access_key_id'],
+                       aws_secret_access_key=kwargs['aws_secret_access_key'])
 
 
 def get_queue(queue_name, conn):
