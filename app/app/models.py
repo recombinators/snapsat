6611d620012c79c9d756e96e3f623dc25ea064db
jacquestardie <hi@jacquestardie.org>
2015-03-24 12:13:07 -0700

    update models and create queries

--- app/app/models.py
+++ app/app/models.py
@@ -1,5 +1,5 @@
 # import sqlalchemy as sa
-from sqlalchemy import Column, Index, Integer, UnicodeText, func
+from sqlalchemy import Column, Index, Integer, UnicodeText, func, DateTime, Float, or_, and_
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -26,76 +26,32 @@ class PathAndRow_Model(Base):
                             .ST_MakePoint(lon, lat), 4236), func
                         .ST_SetSRID(cls.geom, 4236))).all()
                     )
-            # import pdb; pdb.set_trace()
             return scene
         except:
             return u'----'
 
 
-# class Incidents_Model(Base):
-#     """SQLalchemy model for incident table."""
-#     __tablename__ = 'incidents'
-#     gid = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
-#     units = sa.Column(sa.UnicodeText, nullable=False)
-#     date_time = sa.Column(sa.DateTime, nullable=False)
-#     incident_type = sa.Column(sa.UnicodeText, nullable=False)
-#     address = sa.Column(sa.UnicodeText, nullable=False)
-#     incident_number = sa.Column(sa.UnicodeText, nullable=False)
-#     latitude = sa.Column(sa.UnicodeText, nullable=False)
-#     longitude = sa.Column(sa.UnicodeText, nullable=False)
-#     major_category = sa.Column(sa.UnicodeText, nullable=False)
-#     minor_category = sa.Column(sa.UnicodeText, nullable=False)
-#     the_geom = sa.Column(sa.UnicodeText, nullable=False)
+class SceneList_Model(Base):
+    '''Model for AWS S3 scene list.'''
+    __tablename__ = 'path_row'
+    entityid = Column(UnicodeText, primary_key=True)
+    acquisitiondate = Column(DateTime, nullable=False)
+    cloudcover = Column(Float, nullable=False)
+    path = Column(Integer, nullable=False)
+    row = Column(Integer, nullable=False)
+    download_url = Column(UnicodeText, nullable=False)
 
-#     @classmethod
-#     def cat_circle(cls, lat, lon, major_cat, radius=0.0067):
-#         """Outputs list of incidents filtered by Major Category"""
-#         return (DBSession.query(cls.date_time)
-#                 # .order_by(func.random())
-#                 .filter(func.ST_Point_Inside_Circle(cls.the_geom, lon, lat,
-#                                                     radius),
-#                         cls.major_category == major_cat).all()
-#                 )
-
-#     @classmethod
-#     def percentage(cls, list_of_times):
-#         """Given a list of epoch times, return a dict with percent increase
-#         as HTML and the number of incidents, over the last year."""
-#         try:
-#             # Define one year ago as 365 days before the most recent incident
-#             one_year_ago_epoch = list_of_times[-1]-365
-#             length_list = len(list_of_times)
-#             # print "length: {}".format(length_list)
-
-#             # Count number of incidents before one year ago.
-#             incidents_prior = 0
-#             for time in list_of_times:
-#                 if time < one_year_ago_epoch:
-#                     incidents_prior += 1
-#                 if time > one_year_ago_epoch:
-#                     break
-#             # Calculate percentage change
-#             incidents_last_year = length_list-incidents_prior
-#             years_prior = (one_year_ago_epoch-list_of_times[0])/365
-#             incidents_per_year_prior = incidents_prior/years_prior
-#             incidents_per_year_last_year = incidents_last_year
-#             percent = (
-#                 100*(incidents_per_year_last_year-incidents_per_year_prior)
-#                 / incidents_per_year_prior)
-
-#             # Prepare HTML with appropriate class so font color changes.
-#             if percent >= 0:
-#                 pos_neg = ("pos", "increased")
-#             else:
-#                 pos_neg = ("neg", "decreased")
-#             return_string = (
-#                 '<span class="{}">{} {}%</span>'.format(pos_neg[0], pos_neg[1],
-#                                                         abs(round(percent, 2)))
-#                 )
-#         except:     # In case that no incidents are found in certain area
-#             return_string = '<span class="no_change">---- 0.00%   </span>'
-#             incidents_last_year = 0
-#         return {'string': return_string, 'year_count': incidents_last_year, 'prior_rate': incidents_per_year_prior}
-
-
-# Index('my_index', MyModel.name, unique=True, mysql_length=255)
+    @classmethod
+    def scenelist(cls, pr_output):
+        # return ['Hi!', 'Bye!']
+        # new = []
+        # for x in pr_output:
+        #     new.append('and_(cls.row == {}, cls.path == {})'.format(x.row, x.path))
+        # new_q = str.join(', ', new)
+        output =  DBSession.query(cls).filter(or_(
+            # and_(cls.row == {}, cls.path == {}),
+            and_(cls.row == pr_output[1].row, cls.path == pr_output[1].path),
+            and_(cls.row == pr_output[0].row, cls.path == pr_output[0].path))).all()
+        # output =  DBSession.query(cls).filter(or_(new_q))
+        import pdb; pdb.set_trace()
+        return output
--- app/app/views.py
+++ app/app/views.py
@@ -1,46 +1,19 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 from sqlalchemy.exc import DBAPIError
-from .models import DBSession, PathAndRow_Model
+from .models import DBSession, PathAndRow_Model, SceneList_Model
+
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
     '''Index page.'''
     lat = float(request.params.get('lat', 47.614848))
     lng = float(request.params.get('lng', -122.3359059))
-
-    scene = PathAndRow_Model.pathandrow(lat, lng)
-
-    return {'scene': scene}
+    scenes = SceneList_Model.scenelist(PathAndRow_Model.pathandrow(lat, lng))
+    return {'scenes': scenes}
 
 
 @view_config(route_name='submit', renderer='json')
 def submit(request):
     '''Accept a post request.'''
     return {}
-
-
-
-# @view_config(route_name='home', renderer='templates/mytemplate.pt')
-# def my_view(request):
-#     try:
-#         one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-#     except DBAPIError:
-#         return Response(conn_err_msg, content_type='text/plain', status_int=500)
-#     return {'one': one, 'project': 'test'}
-
-# conn_err_msg = """\
-# Pyramid is having a problem using your SQL database.  The problem
-# might be caused by one of the following things:
-
-# 1.  You may need to run the "initialize_test_db" script
-#     to initialize your database tables.  Check your virtual
-#     environment's "bin" directory for this script and try to run it.
-
-# 2.  Your database server may not be running.  Check that the
-#     database server referred to by the "sqlalchemy.url" setting in
-#     your "development.ini" file is running.
-
-# After you fix the problem, please restart the Pyramid application to
-# try it again.
-# """
