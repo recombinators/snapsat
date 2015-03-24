3546a96ac4f31232fb4c3f44dd17c6254b2ad920
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-24 12:37:31 -0700

    #15 kwargs

--- app/app/sqs.py
+++ app/app/sqs.py
@@ -2,7 +2,7 @@ from boto.sqs import connect_to_region
 from boto.sqs.message import Message
 
 
-def make_connection(kwargs):
+def make_connection(**kwargs):
     '''Make a connection to an AWS account. Kwargs is a dictionary of the AWS
        region, AWS access key id, and AWS secret access key'''
     return connect_to_region(kwargs['region'],
@@ -40,7 +40,7 @@ def queue_size(queue):
     return queue.count()
 
 
-def build_job_message(kwargs):
+def build_job_message(**kwargs):
     '''Build a meesage to add to the jobs queue.'''
     job_message = Message()
     job_message.set_body('job')
@@ -81,7 +81,7 @@ def build_job_message(kwargs):
     return job_message
 
 
-def build_result_message(kwargs):
+def build_result_message(**kwargs):
     '''Build a message to add to the results queue.'''
     result_message = Message()
     result_message.set_body('result')
