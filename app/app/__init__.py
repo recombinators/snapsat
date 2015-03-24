77ae8c80c2248c5048e482cfe1e196b126a4359b
jacquestardie <hi@jacquestardie.org>
2015-03-23 17:10:06 -0700

    mock ajax routes and responses

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -8,5 +8,6 @@ def main(global_config, **settings):
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('index', '/')
+    config.add_route('submit', '/submit')
     config.scan()
     return config.make_wsgi_app()
--- app/app/views.py
+++ app/app/views.py
@@ -3,4 +3,11 @@ from pyramid.view import view_config
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
+    '''Index page.'''
+    return {}
+
+
+@view_config(route_name='submit', renderer='json')
+def submit(request):
+    '''Accept a post request.'''
     return {}
