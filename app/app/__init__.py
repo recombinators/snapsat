7dff07484ec6a4c905fe43505bd1da14e3b30ffa
jacquestardie <hi@jacquestardie.org>
2015-03-24 21:53:18 -0700

    pull connection string from dev.ini

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,3 +1,4 @@
+import os
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
@@ -6,9 +7,11 @@ from .models import DBSession, Base
 
 def main(global_config, **settings):
     '''This function returns a Pyramid WSGI application.'''
+    settings['sqlalchemy.url'] = os.environ.get('DATABASE_URL')
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
+    import pdb; pdb.set_trace()
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
