4dff440f498ac5a574a06970b6b43ff337923e51
jacquestardie <hi@jacquestardie.org>
2015-03-24 21:54:33 -0700

    remove pdb

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -11,7 +11,6 @@ def main(global_config, **settings):
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
-    import pdb; pdb.set_trace()
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
