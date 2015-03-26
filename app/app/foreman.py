12537f2df358c17b4dee99370a10a3c45fde3537
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-26 13:36:29 -0700

    #3, #15 update foreman function to connect to queue and check size

--- app/app/foreman.py
+++ app/app/foreman.py
@@ -4,18 +4,21 @@ from sqs import (make_SQS_connection, get_queue, build_job_message, send_message
                  queue_size,)
 from operator import attrgetter
 
+JOBS_QUEUE = 'landsat_jobs_queue'
+
 
 def make_EC2_connection(region_name, aws_access_key_id, aws_secret_access_key):
     '''Make EC2 connection to AWS'''
-
     return EC2Connection(aws_access_key_id=aws_access_key_id,
                          aws_secret_access_key=aws_secret_access_key,
                          region=get_region(region_name))
 
 
-def foreman(conn):
-    '''Check status of queue and spaqn/kill workers as needed'''
-
+def foreman(conn, region_name, aws_access_key_id, aws_secret_access_key):
+    '''Check status of queue and spawn/kill workers as needed'''
+    make_SQS_connection(region_name, aws_access_key_id, aws_secret_access_key)
+    jobs_queue = get_queue(conn, JOBS_QUEUE)
+    number_queued_jobs = queue_size(jobs_queue)
     pass
 
 
@@ -30,7 +33,6 @@ def kill_worker(conn):
 def list_worker_instances(conn, worker_type):
     '''Return list of worker instances in order of lauch time, most recent
        first.'''
-
     filters = {'tag:Name': worker_type}
     instances = conn.get_only_instances(filters=filters)
     instances.sort(key=attrgetter('launch_time'), reverse=True)
@@ -42,10 +44,9 @@ if __name__ == '__main__':
 
     AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-    JOBS_QUEUE = 'landsat_jobs_queue'
     REGION = 'us-west-2'
 
-    conn = make_connection(REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
+    conn = make_EC2_connection(REGION, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
     worker_type = 'landsatAWS_worker'
     workers = list_worker_instances(conn, worker_type)
     import pdb; pdb.set_trace()
