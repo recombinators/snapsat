992d02d38934320b61f5ac1c219dc416a3a614a6
Mark Saiget <saiget@gmail.com>
2015-03-26 15:57:36 -0700

    Add preview urls.

--- app/app/views.py
+++ app/app/views.py
@@ -59,6 +59,14 @@ def done(request):
     UserJob_Model.set_job_status(pk, status, url)
 
 
+def preview_url(scene, band1, band2, band3):
+    '''get link for preview url'''
+    # root = 'ec2-52-11-232-129.us-west-2.compute.amazonaws.com'
+    root = 'http://ec2-52-11-232-129.us-west-2.compute.amazonaws.com'
+    url = '{}/{}/{}/{}/{}/preview.png'.format(root, scene, band1, band2, band3)
+    return url
+
+
 @view_config(route_name='ajax', renderer='json')
 def scene_options_ajax(request):
     """View for ajax request returns dict with all available scenes centered on
@@ -70,12 +78,14 @@ def scene_options_ajax(request):
 
     scenes_dict = []
     for i, scene in enumerate(scenes):
+        s_preview_url = preview_url(scene.entityid, 4, 3, 2)
         scenes_dict.append({'acquisitiondate': scene.acquisitiondate.strftime('%Y %B %d'),
                             'cloudcover': scene.cloudcover,
                             'download_url': scene.download_url,
                             'entityid': scene.entityid,
                             'path': scene.path,
-                            'row': scene.row})
+                            'row': scene.row,
+                            'preview': s_preview_url})
 
     scenes_dict.sort(key=operator.itemgetter('acquisitiondate'), reverse=True)
 
