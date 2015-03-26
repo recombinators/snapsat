9e9fac0e115c1ad13370dbe4524de78b5751ac9a
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-25 22:45:54 -0700

    #31 list is updating but button is broken

--- app/app/static/bundle.js
+++ app/app/static/bundle.js
@@ -41,10 +41,40 @@ map.on('moveend', function() {
         },
     }).done(function(json) {
         // Update site contents with new data
-        $(".test_lat").contents().replaceWith(json.lat);
-        $(".test_lng").contents().replaceWith(json.lng);
-        $("#available-scenes").contents().replaceWith(json.scenes);
+        data = json.scenes
+        $('.scene_list').html('');
+            for (i in data){
+                var pad = "000";
+                var r = data[i].row;
+                var p = data[i].path;
+                var r_result = (pad+r).slice(-pad.length);
+                var p_result = (pad+p).slice(-pad.length);
 
+                $('.scene_list').append(
+                    "<h3 class='js-trigger mbn'>" + data[i].acquisitiondate + "</h3>" +
+                    "<div class='details'>" +
+                        "<p>Path:" + data[i].path + "</p>" +
+                        "<p>Row:" + data[i].row + "</p>" +
+                        "<p>Cloud coverage:" + data[i].cloudcover + "</p>" +
+                        "<img src='https://s3-us-west-2.amazonaws.com/landsat-pds/L8/" + p_result + "/" + r_result + "/" + data[i].entityid + "/" + data[i].entityid + "_thumb_large.jpg'>" +
+                        "<form action='/request/" + data[i].entityid + "' method='post' id='request_form'>" +
+                        "<div id='combinations' class='display-inline-block'>" +
+                        "<h4 class='customband'>Select your custom band combinations</h4>" +
+                        "<div class='radiobox'>" +
+                        "<input type='radio' name='band_combo' value='432' checked>" + 
+                            "<label>Normal Colors: 4, 3, 2</label><br>" +
+                        "<input type='radio' name='band_combo' value='543'>" +
+                            "<label>See the Heat: 5, 4, 3</label><br>" +
+                        "<input type='radio' name='band_combo' value='532'>" +
+                            "<label>Veggie Popping: 5, 3, 2</label><br>" +
+                        "</div>" +
+                        "</div>" +    
+                        "<button form='request_form' formmethod='post' type='submit'>" +
+                            "Request" +
+                            "</button>" +
+                        "</form>" +
+                    "</div>");
+                };
     });
 });
 
