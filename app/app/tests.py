a2cd45d361393912fb45200df54626af5f9c5844
poolbath1 <eyftickid@poolbath1.33mail.com>
2015-03-27 12:23:56 -0700

    self running map

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,55 +1,122 @@
 import unittest
 import transaction
-
+import os
+import app
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
 from pyramid import testing
 
 from .models import DBSession
 
 
+DEFAULT_WAIT = 5
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
+
+
 class TestMyViewSuccessCondition(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
     def test_passing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'app')
+        pass
 
 
 class TestMyViewFailureCondition(unittest.TestCase):
     def setUp(self):
         self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
 
     def tearDown(self):
         DBSession.remove()
         testing.tearDown()
 
     def test_failing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info.status_int, 500)
+        pass
+
+
+class FunctionalTest(unittest.TestCase):
+
+    def setUp(self):
+        self.config = testing.setUp()
+
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(DEFAULT_WAIT)
+
+    def tearDown(self):
+        DBSession.remove()
+        testing.tearDown()
+        self.browser.quit()
+
+
+class HomePageTest(FunctionalTest):
+
+    def test_home_page_loads(self):
+        #Billy sees the landsat.club homepage and rejoices.
+        self.browser.get('http://develop.landsat.club')
+        self.assertIn('Snapsat',
+                      self.browser.page_source)
+        zoom_in = self.browser.find_element_by_class_name("leaflet-control-zoom-in")
+        zoom_out = self.browser.find_element_by_class_name("leaflet-control-zoom-out")
+        map = self.browser.find_element_by_id("map")
+        map.send_keys(Keys.ARROW_DOWN)
+        sleep(.75)
+        map.send_keys(Keys.ARROW_DOWN)
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+
+        map.send_keys(Keys.ARROW_DOWN)
+        sleep(.75)
+        map.send_keys(Keys.ARROW_DOWN)
+        sleep(.75)
+        map.send_keys(Keys.ARROW_DOWN)
+        sleep(.75)
+        map.send_keys(Keys.ARROW_RIGHT)
+        map.send_keys(Keys.ARROW_LEFT)
+        map.send_keys(Keys.ARROW_DOWN)
+        zoom_in.click()
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.75)
+        zoom_in.click()
+        sleep(.75)
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.5)
+        zoom_in.click()
+        sleep(.75)
+        zoom_in.click()
+        sleep(5)
+        self.browser.find_element_by_class_name('leaflet-control-mapbox-geocoder-toggle').click()
+        self.browser.find_element_by_xpath('//*[@id="map"]/div[2]/div[1]/div[2]/div[2]/form/input').send_keys('10010', Keys.RETURN)
+        
+        
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+        sleep(.75)
+        zoom_out.click()
+        
+        sleep(600)
