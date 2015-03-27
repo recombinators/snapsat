23c4c9fd077b38eec3db3acc06455e4b18a06c86
Jakealope <jake.anderson486@gmail.com>
2015-03-26 18:29:56 -0700

    #32 fiddled with it

--- app/app/static/bundle.js
+++ app/app/static/bundle.js
@@ -69,18 +69,19 @@ map.on('moveend', function() {
         //                     "<label>Veggie Popping: 5, 3, 2</label><br>" +
         //                 "</div>" +
         //                 "</div>" +    
-        //                 "<button form='request_form' formmethod='post' type='submit'>" +
-        //                     "Request" +
-        //                     "</button>" +
+        //                 
         //                 "</form>" +
         //             "</div>");
-        //     };
+        //    };
+            "<button form='request_form' formmethod='post' type='submit'>" +
+                            "Request" +
+                            "</button>" +
         $('#date-select').html('');
         for (i in data){
             $('#date-select').append(
                  '<option value="scene-{{loop.index}}">' + data[i].acquisitiondate + " | Cloud Cover " + data[i].cloudcover + ' %</option>'
                 );
-            
+
         };
     });
 });
