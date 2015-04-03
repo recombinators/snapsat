2327d245f613c3ddcaf0a3ead0689dcb109654c6
jacquestardie <hi@jacquestardie.org>
2015-04-03 09:58:27 -0700

    add create route + reorganize

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -2,15 +2,20 @@ from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    '''This function returns a Pyramid WSGI application.'''
+    """
+    Configure and return a WSGI application.
+    """
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.add_static_view('static', 'static', cache_max_age=3600)
+
+    # Define routes
     config.add_route('index', '/')
+    config.add_route('create', '/create')
     config.add_route('request_scene', '/request/{scene_id}')
     config.add_route('request_preview', '/request_p/{scene_id}')
-    config.add_route('done', '/done')
     config.add_route('scene_status', '/scene/{scene_id}')
     config.add_route('ajax', '/ajax')
+
     config.scan()
     return config.make_wsgi_app()
