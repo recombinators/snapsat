8a6144fc3918856cb2259f65f9ee5cc9cfaf1fdc
Mark Saiget <saiget@gmail.com>
2015-04-06 12:11:08 -0700

    Add random functionality to map move.

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,10 @@
 from locust import HttpLocust, TaskSet, task
+from bs4 import BeautifulSoup
+from requests import Session
+import random
 
 
 class UserBehavior(TaskSet):
-    tasks = []
-
     def on_start(self):
         pass
 
@@ -13,22 +14,30 @@ class UserBehavior(TaskSet):
 
     @task
     def move_map(self):
+        lat = random.uniform(-1, 1)
+        lon = random.uniform(-1, 1)
+        response = self.client.post(
+            url="/ajax",
+            data={'lat': lat, 'lng': lng,}
+            )
+
         self.client.get("")
 
-    @task
-    def select_scene(self):
-        # Get url
-
-
-        self.client.get()
-
         @task
-        def render_preview(self):
+        def select_scene(self):
+            # Get url
+            soup = BeautifulSoup(self.client.get(""))
+
+
             self.client.get()
 
-        @task
-        def render_full(self):
-            self.client.get()
+            @task
+            def render_preview(self):
+                self.client.get()
+
+            @task
+            def render_full(self):
+                self.client.get()
 
 
 class WebsiteUser(HttpLocust):
