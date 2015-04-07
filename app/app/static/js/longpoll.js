64289073db4f6cbeb332c7a85b502498b9d4b53c
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-07 14:15:26 -0700

    #67, #68 generalize polling function arguments get id tags for elements in DOM that need updating by polls

--- app/app/static/js/longpoll.js
+++ app/app/static/js/longpoll.js
@@ -3,45 +3,47 @@ var $ = require('jquery');
 // Start polling for preview and full render job status and take action when document is ready
 $( document ).ready(function() {
 
-    $(".nopreview").each(function(){
-        var previewIntervalTime = 1000;
-        var previewIntervalID = setInterval(startPreviewPoll(previewIntervalID), previewIntervalTime);
+    $(".nopreview").each(function(i, obj){
+        var jobId = $(obj).attr('id');
+        var intervalTime = 1000;
+        var intervalID = setInterval(startPreviewPoll(jobId, intervalID), intervalTime);
     });
     
     $(".nofull").each(function(){
+        var jobId = $(obj).attr('id');
         var statusIntervalTime = 10000;
-        var statusIntervalID = setInterval(startStatusPoll(statusIntervalID), statusIntervalTime);
+        var statusIntervalID = setInterval(startStatusPoll(jobId, intervalID), intervalTime);
     });
 
 });
 
 // Stop polling for a preview when 
-function stopPreviewPoll(data, previewintervalID){
+function stopPreviewPoll(data, intervalID){
     if(data.bool === false){
         clearInterval(intervalID);
     }
 }
 
 // Start polliing for preview status
-function startPreviewPoll(previewIntervalID){
+function startPreviewPoll(jobId, intervalID){
     $.ajax({
         url: "/preview_poll", 
         dataType: "json"
     }).done(function(data){
         console.log(data.bool);
-        stopPreviewPoll(data.bool, previewIntervalID);
+        stopPreviewPoll(data.bool, intervalID);
     });
 }
 
 // Stop polliing for full render status when
-function stopStatusPoll(data, statusintervalID){
+function stopStatusPoll(data, intervalID){
     if(data.bool === false){
         clearInterval(intervalID);
     }
 }
 
 // Start polliing for full render status
-function startStatusPoll(statusintervalID){
+function startStatusPoll(jobId, intervalID){
     $.ajax({
         url: "/status_poll", 
         dataType: "json"
