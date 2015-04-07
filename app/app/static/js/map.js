034e4794eefc876e420c6093a4594a57dff3e436
jacquestardie <hi@jacquestardie.org>
2015-04-06 18:04:30 -0700

    first pass at cleaning up ajax code

--- app/app/static/js/map.js
+++ app/app/static/js/map.js
@@ -1,8 +1,8 @@
 require('mapbox.js');
 var $ = require('jquery');
+var hb = require('handlebars');
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
-
 
 // Create a basemap
 var map = L.mapbox.map('map', 'jacques.lh797p9e', {zoomControl: true});
@@ -13,58 +13,56 @@ map.addControl(L.mapbox.geocoderControl('mapbox.places'));
 
 // Once a user finishes moving the map, send an AJAX request to Pyramid
 // which will repopulate the HTML with an updated list of the Landsat
-// scenes present.
+// scenes appropriate to the lat/lon.
 map.on('moveend', function() {
 
-    // Define the center of the map.
+    // Name the center of the map.
     var center = map.getCenter(),
         lat = center.lat,
         lng = center.lng;
 
-    // Submit a post request with the relevant information.
+    // On `moveend`, submit a post request.
     $.ajax({
         url: "/scene_options_ajax",
         dataType: "json",
         data: {'lat': lat, 'lng': lng, }
     }).done(function(json) {
 
-        scenes = json.scenes_date;
-        scenes_pr = json.scenes_path_row;
+        scenes = json.scenes;
         
-        $('#pathrowgrouping').html('');
-            for (var i in scenes_pr) {
-                var num = i;
-                var n = num.toString();
-                var id = 'tab'.concat(n);
+        // Empty the contents of .sceneList
+        $('.sceneList').children().remove();
 
-                // $('#pathrowgrouping').append(
-                //     $('<h3 class="mvm">' + scenes_path_row[0].sliced + "</h3>")
-                // );
+        // Iterate through each set of scenes and add them to the DOM.
+        for (var i in scenes) {
+            // Grab the scene's index and concatenate it with `tab`.
+            // `newid` will be used
+            var id = 'tab'.concat(i.toString()),
+                newid = '#'.concat(id);
 
-                $('#pathrowgrouping').append(
-                    $('<table></table>').attr('id', id)
-                );
+            var scene = scenes[i],
 
-                var scenes_path_row = scenes_pr[i];
-                var newid = '#'.concat(id);
+            // Append a heading and a table for each set of scenes.
+            $('.sceneList').append('<h3></h3>'.attr('id', id).val('Test'));
+            $('.sceneList').append('<table></table>').attr('id', id);
 
-                $(newid).html('');
+            $(newid).html('');
+            $(newid).append(
+                "<thead><tr><th>Date acquired</th><th>Path</th><th>Row</th><th>Cloud cover</th><th>ID</th></tr></thead>"
+            );
+
+            // Iterate through each scene in the scene list.
+            for (var k in scene) {
+                // Add a scene
                 $(newid).append(
-                    "<thead><tr><th>Date acquired</th><th>Path</th><th>Row</th><th>Cloud cover</th><th>ID</th></tr></thead>"
-                );
-                // $(newid).append(
-                //     "<tr><td><strong>" + scenes_path_row[0].sliced + "</strong></td></tr>"
-                // );
-                for (var k in scenes_path_row) {
-                    $(newid).append(
-                        "<tr>" +
-                            "<td>" + scenes_path_row[k].acquisitiondate + "</td>" +
-                            "<td>" + scenes_path_row[k].path + "</td>" +
-                            "<td>" + scenes_path_row[k].row + "</td>" +
-                            "<td>" + scenes_path_row[k].cloudcover + "</td>" +
-                            "<td><a href='/scene/" + scenes_path_row[k].entityid + "'>" + scenes_path_row[k].entityid + "</a></td>" +
-                        "</tr>");
-                }
+                    "<tr>" +
+                        "<td>" + scene[k].acquisitiondate + "</td>" +
+                        "<td>" + scene[k].path + "</td>" +
+                        "<td>" + scene[k].row + "</td>" +
+                        "<td>" + scene[k].cloudcover + "</td>" +
+                        "<td><a href='/scene/" + scene[k].entityid + "'>" + scene[k].entityid + "</a></td>" +
+                    "</tr>");
             }
+        }
     });
 });
