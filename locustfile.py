3d6d7c4061380f87f0d25ff98ae0147772e6b0e6
Mark Saiget <saiget@gmail.com>
2015-04-06 20:56:14 -0700

    Add on_start back

--- locustfile.py
+++ locustfile.py
@@ -1,9 +1,9 @@
 from locust import HttpLocust, TaskSet, task
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 import random
 import json
-import urllib3
-urllib3.disable_warnings()
+# import urllib3
+# urllib3.disable_warnings()
 lat = 47.614848
 lng = -122.3359059
 
@@ -12,19 +12,11 @@ class UserBehavior(TaskSet):
     def on_start(self):
         pass
 
-    @task(1)
+    @task
     def index(self):
         self.client.get("/")
 
-    # @task(1)
-    # def stop(self):
-    #     self.interrupt()
-    # @task(1)
-    # def create(self):
-    #     print "create"
-    #     self.response = self.client.get("/create")
-
-    @task(3)
+    @task
     class SubTaskCreate(TaskSet):
         # def __init__():
             # self.response = self.client.get("/create")
@@ -37,24 +29,35 @@ class UserBehavior(TaskSet):
             #     data={'lat': self.lat, 'lng': self.lng}
             #     )
 
-        @task(10)
-        def move_map(self):
-            self.lat = random.uniform(-1, 1) + lat
-            self.lng = random.uniform(-1, 1) + lng
+        # @task(10)
+        # def move_map(self):
+        #     self.lat = random.uniform(-1, 1) + lat
+        #     self.lng = random.uniform(-1, 1) + lng
+        #     # response = self.client.get("/create")
+        #     # print self.lat, self.lng
+        #     response = self.client.post(
+        #         url="/scene_options_ajax",
+        #         json="True",
+        #         data={'lat': self.lat, 'lng': self.lng}
+        #         )
+        #     import pdb; pdb.set_trace()
+        def on_start(self):
+            self.lat = random.uniform(-10, 10) + lat
+            self.lng = random.uniform(-10, 10) + lng
             # response = self.client.get("/create")
             # print self.lat, self.lng
-            response = self.client.post(
-                url="/scene_options_ajax",
-                json="True",
-                data={'lat': self.lat, 'lng': self.lng}
-                )
-            import pdb; pdb.set_trace()
-        # @task(5)
-        # def preview(self):
-        #     self.move_map()
-            json_data = json.loads(response.text)
+            self.response = self.client.post(
+                                        url="/scene_options_ajax",
+                                        json="True",
+                                        data={'lat': self.lat, 'lng': self.lng}
+                                             )
+
+
+        @task(5)
+        def preview(self):
+            json_data = json.loads(self.response.text)
             random_num = random.randint(0, len(json_data['scenes']) - 1)
-            random_url = json_data["scenes"][random_num]["download_url"]
+            # random_url = json_data["scenes"][random_num]["download_url"]
             scene_id = json_data["scenes"][random_num]["entityid"]
             # print random_url
             # self.response = self.client.get(random_url)
@@ -79,21 +82,6 @@ class UserBehavior(TaskSet):
                 #     self.scene_id = json_data["scenes_date"][random_num]["entityid"]
                 #     print random_url
                 #     self.response = self.client.get(random_url)
-        # @task(1)
-        # def select_scene(self):
-        #     # Get url
-        #     soup = BeautifulSoup(self.client.get(""))
-
-
-        #     self.client.get()
-
-        #     @task
-        #     def render_preview(self):
-        #         self.client.get()
-
-        #     @task
-        #     def render_full(self):
-        #         self.client.get()
 
 
 class WebsiteUser(HttpLocust):
