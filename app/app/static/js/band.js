80c9917f9c5f6dcc2b176b16f6cecb50c872e7ed
Mark Saiget <saiget@gmail.com>
2015-04-08 13:10:03 -0700

    Add comments to band.js script

--- app/app/static/js/band.js
+++ app/app/static/js/band.js
@@ -1,7 +1,12 @@
+// A jQuery script to dynamically limit band selections to 1-7 and 9. It also
+// does not allow the user to select the same band more than once.
 $("#band1").on('focus', function (){
+    // Delete existing options
     $("#band1").empty()
+    // Make array with values from other select boxes and 8, so user cannot
+    // select an existing selection or 8
     var existingValues = [parseInt($("#band2").val()), parseInt($("#band3").val()), 8];
-    console.log(existingValues);
+    // Add options if they are not in exisitingValues array
     for (i=1; i < 10; i++) {
         if ($.inArray(i, existingValues) < 0) {
             $("#band1").append("<option value='" + i + "'>" + i + "</option>")
@@ -9,9 +14,12 @@ $("#band1").on('focus', function (){
     }
 });
 $("#band2").on('focus', function (){
+    // Delete existing options
     $("#band2").empty()
+    // Make array with values from other select boxes and 8, so user cannot
+    // select an existing selection or 8
     var existingValues = [parseInt($("#band1").val()), parseInt($("#band3").val()), 8];
-    console.log(existingValues);
+    // Add options if they are not in exisitingValues array
     for (i=1; i < 10; i++) {
         if ($.inArray(i, existingValues) < 0) {
             $("#band2").append("<option value='" + i + "'>" + i + "</option>")
@@ -19,9 +27,12 @@ $("#band2").on('focus', function (){
     }
 });
 $("#band3").on('focus', function (){
+    // Delete existing options
     $("#band3").empty()
+    // Make array with values from other select boxes and 8, so user cannot
+    // select an existing selection or 8
     var existingValues = [parseInt($("#band1").val()), parseInt($("#band2").val()), 8];
-    console.log(existingValues);
+    // Add options if they are not in exisitingValues array
     for (i=1; i < 10; i++) {
         if ($.inArray(i, existingValues) < 0) {
             $("#band3").append("<option value='" + i + "'>" + i + "</option>")
