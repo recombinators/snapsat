5b40c156c55c22f3efc8055a0c4127e890462635
Mark Saiget <saiget@gmail.com>
2015-04-08 14:39:29 -0700

    Remove # from link

--- app/app/views.py
+++ app/app/views.py
@@ -132,7 +132,7 @@ def request_preview(request):
     """
     # Add preview render job to apprpriate queues
     add_to_queue_preview(request)
-    return HTTPFound(location='/scene/{}#445'.format(
+    return HTTPFound(location='/scene/{}'.format(
         request.matchdict['scene_id']))
 
 
