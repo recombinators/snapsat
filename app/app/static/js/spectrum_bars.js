6350c728870151376aca631a9b1e3bdffde381e9
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-08 18:52:01 -0700

    #84 bar fill color matches band mapping

--- app/app/static/js/spectrum_bars.js
+++ app/app/static/js/spectrum_bars.js
@@ -1,19 +1,52 @@
 function graph() {
-    // var colore = d3.scale.ordinal()
-    //     .domain(d3.extent(dataset, function (d) { return d.Peso; }))
-    //     .range(["#fee0d2","#fcbba1","#fc9272","#fb6a4a","#ef3b2c","#cb181d","#a50f15","#67000d"]);
-
-    var xLowNorm = 400;
-    var widthNorm = 2294;
-    var waveLengthsTop = [[503, 676, 8], [1362, 1384, 9]];
-    var waveLengthsBottom = [[435, 451, 1], [452, 512, 2], [533, 590, 3], [636, 673, 4], [851, 879, 5], [1566, 1651, 6], [2107, 2294, 7]];
 
     var width = $(".graph").parent().width();
     var height = 20;
     var stroke_width = 1;
     var box_fill = "#FFF";
     var stroke_color = "#000";
+
     var font_size = 8;
+
+    var red_band = Number($(".band-red").html());
+    var green_band = Number($(".band-green").html());
+    var blue_band = Number($(".band-blue").html());
+    var red_color = "red";
+    var green_color = "green";
+    var blue_color = "blue";
+
+    var xLowNorm = 400;
+    var widthNorm = 2294;
+    var waveLengthsTop = [[503, 676, 8, box_fill],
+                          [1362, 1384, 9, box_fill]];
+    var waveLengthsBottom = [[435, 451, 1, box_fill],
+                             [452, 512, 2, box_fill],
+                             [533, 590, 3, box_fill],
+                             [636, 673, 4, box_fill],
+                             [851, 879, 5, box_fill],
+                             [1566, 1651, 6, box_fill],
+                             [2107, 2294, 7, box_fill]];
+
+    for (var i in waveLengthsTop) {
+        if(waveLengthsTop[i][2] == red_band){
+            waveLengthsTop[i][3] = red_color;
+        }else if(waveLengthsTop[i][2] == blue_band){
+            waveLengthsTop[i][3] = blue_color;
+        }else if(waveLengthsTop[i][2] == green_band){
+            waveLengthsTop[i][3] = green_color;
+        }
+    }
+
+    for (var j in waveLengthsBottom) {
+        if(waveLengthsBottom[j][2] == red_band){
+            waveLengthsBottom[j][3] = red_color;
+        }else if(waveLengthsBottom[j][2] == blue_band){
+            waveLengthsBottom[j][3] = blue_color;
+        }else if(waveLengthsBottom[j][2] == green_band){
+            waveLengthsBottom[j][3] = green_color;
+        }
+    }
+
 
     var svg = d3.selectAll(".graph")
         .append("svg")
@@ -26,7 +59,7 @@ function graph() {
         .append("rect")
         .attr("stroke-width", stroke_width)
         .attr("stroke", "#000")
-        .attr("fill", "#FFF")
+        .attr("fill", function (d){ return (d[3]); })
         .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
         .attr("y", 0 + "px")
         .attr("width", function (d){ return (width * (d[1] - d[0]) / (widthNorm - xLowNorm)) + "px"; })
@@ -38,7 +71,7 @@ function graph() {
         .append("rect")
         .attr("stroke-width", stroke_width)
         .attr("stroke", "#000")
-        .attr("fill", "#FFF")
+        .attr("fill", function (d){ return (d[3]); })
         .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
         .attr("y", height + 2 * stroke_width + "px")
         .attr("width", function (d){ return (width * (d[1] - d[0]) / (widthNorm - xLowNorm)) + "px"; })
