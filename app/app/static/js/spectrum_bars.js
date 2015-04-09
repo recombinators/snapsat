8063c132e8d82d062c7d97c5ba3f46d95f817b7d
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-08 17:49:48 -0700

    #84 fix creation of rectangles and spacing between top and bottom bars

--- app/app/static/js/spectrum_bars.js
+++ app/app/static/js/spectrum_bars.js
@@ -5,29 +5,40 @@ function graph() {
 
     var xLowNorm = 435;
     var widthNorm = 2294;
-
-    var waveLengths = [[435, 451, 1], [452, 512, 2], [533, 590, 3], [636, 673, 4], [851, 879, 5], [1566, 1651, 6], [2107, 2294, 7], [503, 676, 8], [1362, 1384, 9]];
+    var waveLengthsTop = [[503, 676, 8], [1362, 1384, 9]];
+    var waveLengthsBottom = [[435, 451, 1], [452, 512, 2], [533, 590, 3], [636, 673, 4], [851, 879, 5], [1566, 1651, 6], [2107, 2294, 7]];
 
     var width = $(".graph").parent().width();
     var height = 20;
-    var sw = 1,
-        z = d3.scale.ordinal().range(["#FFF","#FFF","#FFF","#FFF","#FFF"]);
+    var sw = 1;
 
     var svg = d3.select(".graph").append("svg:svg")
         .attr("width", width)
-        .attr("height", height)
+        .attr("height", 2 * (height + sw))
         .append("svg:g");
-        // .attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
 
     svg.selectAll("rect")
-      .data(waveLengths).enter()
+      .data(waveLengthsTop, function(d){return d;})
+      .enter()
       .append("rect")
       .attr("stroke-width", sw)
       .attr("stroke", "#000")
       .attr("fill", "#FFF")
-      .attr("x", function (color, index){ return (width * ((waveLengths[index][0] - xLowNorm) / widthNorm)) + "px"; })
+      .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
       .attr("y", 0 + "px")
-      .attr("width", function (color, index){ return (width * (waveLengths[index][1] - waveLengths[index][0]) / (widthNorm - xLowNorm)) + "px"; })
+      .attr("width", function (d){ return (width * (d[1] - d[0]) / (widthNorm - xLowNorm)) + "px"; })
+      .attr("height", height + "px");
+
+    svg.selectAll("rect")
+      .data(waveLengthsBottom, function(d){return d;})
+      .enter()
+      .append("rect")
+      .attr("stroke-width", sw)
+      .attr("stroke", "#000")
+      .attr("fill", "#FFF")
+      .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
+      .attr("y", height + 2* sw + "px")
+      .attr("width", function (d){ return (width * (d[1] - d[0]) / (widthNorm - xLowNorm)) + "px"; })
       .attr("height", height + "px");
 
 }
