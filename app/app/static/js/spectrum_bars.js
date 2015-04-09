819d88d80f3b02df34f10fbb14cf3b8a0403d177
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-09 12:03:39 -0700

    #84 reference spectrum bar at top of page, text removed from preview spectrum bars

--- app/app/static/js/spectrum_bars.js
+++ app/app/static/js/spectrum_bars.js
@@ -1,4 +1,4 @@
-function graph(graphId) {
+function graph(graphId, type) {
 
     var id = '#'.concat(graphId);
     var width = $(id).parent().width();
@@ -12,15 +12,27 @@ function graph(graphId) {
     var red_band = Number($(id).parent().find($(".band-red")).html());
     var green_band = Number($(id).parent().find($(".band-green")).html());
     var blue_band = Number($(id).parent().find($(".band-blue")).html());
-    var red_color = "red";
-    var green_color = "green";
-    var blue_color = "blue";
+    var red_color = "#c02137";
+    var green_color = "#4e9c62";
+    var blue_color = "#00668e";
 
     var xLowNorm = 400;
     var widthNorm = 2294;
-    var waveLengthsTop = [[503, 676, 8, box_fill],
+
+    if(type == "reference"){
+        waveLengthsTop = [[503, 676, 8, "#008ea2"],
+                          [1362, 1384, 9, "#7f87b5"]];
+        waveLengthsBottom = [[435, 451, 1, "#6ca4d3"],
+                             [452, 512, 2, blue_color],
+                             [533, 590, 3, green_color],
+                             [636, 673, 4, red_color],
+                             [851, 879, 5, "#c5a3be"],
+                             [1566, 1651, 6, "#d49979"],
+                             [2107, 2294, 7, "#999b98"]];
+    }else if(type == "preview"){
+        waveLengthsTop = [[503, 676, 8, box_fill],
                           [1362, 1384, 9, box_fill]];
-    var waveLengthsBottom = [[435, 451, 1, box_fill],
+        waveLengthsBottom = [[435, 451, 1, box_fill],
                              [452, 512, 2, box_fill],
                              [533, 590, 3, box_fill],
                              [636, 673, 4, box_fill],
@@ -28,25 +40,28 @@ function graph(graphId) {
                              [1566, 1651, 6, box_fill],
                              [2107, 2294, 7, box_fill]];
 
-    for (var i in waveLengthsTop) {
-        if(waveLengthsTop[i][2] == red_band){
-            waveLengthsTop[i][3] = red_color;
-        }else if(waveLengthsTop[i][2] == blue_band){
-            waveLengthsTop[i][3] = blue_color;
-        }else if(waveLengthsTop[i][2] == green_band){
-            waveLengthsTop[i][3] = green_color;
+        for (var i in waveLengthsTop){
+            if(waveLengthsTop[i][2] == red_band){
+                waveLengthsTop[i][3] = red_color;
+            }else if(waveLengthsTop[i][2] == blue_band){
+                waveLengthsTop[i][3] = blue_color;
+            }else if(waveLengthsTop[i][2] == green_band){
+                waveLengthsTop[i][3] = green_color;
+            }
+        }
+
+        for (var j in waveLengthsBottom){
+            if(waveLengthsBottom[j][2] == red_band){
+                waveLengthsBottom[j][3] = red_color;
+            }else if(waveLengthsBottom[j][2] == blue_band){
+                waveLengthsBottom[j][3] = blue_color;
+            }else if(waveLengthsBottom[j][2] == green_band){
+                waveLengthsBottom[j][3] = green_color;
+            }
         }
     }
 
-    for (var j in waveLengthsBottom) {
-        if(waveLengthsBottom[j][2] == red_band){
-            waveLengthsBottom[j][3] = red_color;
-        }else if(waveLengthsBottom[j][2] == blue_band){
-            waveLengthsBottom[j][3] = blue_color;
-        }else if(waveLengthsBottom[j][2] == green_band){
-            waveLengthsBottom[j][3] = green_color;
-        }
-    }
+    
 
 
     var svgBar = d3.selectAll(id)
@@ -85,45 +100,55 @@ function graph(graphId) {
         .attr("width", function (d){ return (width * (d[1] - d[0]) / (widthNorm - xLowNorm)) + "px"; })
         .attr("height", height + "px");
 
-    svgBar.selectAll("text")
-        .data(waveLengthsTop, function(d){return d;})
-        .enter()
-        .append("text")
-        .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
-        .attr("y", 0 + "px")
-        .attr("text-anchor", "middle")
-        .attr("dx", function (d){ return ((width * (d[1] - d[0]) / (widthNorm - xLowNorm)) / 2) + "px"; })
-        .attr("dy", height / 2 + font_size / 2)
-        .attr("font-size", font_size)
-        .text(function(d) { return d[2]; });
+    if(type == "reference"){
+        svgBar.selectAll("text")
+            .data(waveLengthsTop, function(d){return d;})
+            .enter()
+            .append("text")
+            .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
+            .attr("y", 0 + "px")
+            .attr("text-anchor", "middle")
+            .attr("dx", function (d){ return ((width * (d[1] - d[0]) / (widthNorm - xLowNorm)) / 2) + "px"; })
+            .attr("dy", height / 2 + font_size / 2)
+            .attr("font-size", font_size)
+            .text(function(d) { return d[2]; });
 
-    svgBar.selectAll("text")
-        .data(waveLengthsBottom, function(d){return d;})
-        .enter()
-        .append("text")
-        .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
-        .attr("y", height + 2 * stroke_width + "px")
-        .attr("text-anchor", "middle")
-        .attr("dx", function (d){ return ((width * (d[1] - d[0]) / (widthNorm - xLowNorm)) / 2) + "px"; })
-        .attr("dy", height / 2 + font_size / 2)
-        .attr("font-size", font_size)
-        .text(function(d) { return d[2]; });
+        svgBar.selectAll("text")
+            .data(waveLengthsBottom, function(d){return d;})
+            .enter()
+            .append("text")
+            .attr("x", function (d){ return (width * ((d[0] - xLowNorm) / widthNorm)) + "px"; })
+            .attr("y", height + 2 * stroke_width + "px")
+            .attr("text-anchor", "middle")
+            .attr("dx", function (d){ return ((width * (d[1] - d[0]) / (widthNorm - xLowNorm)) / 2) + "px"; })
+            .attr("dy", height / 2 + font_size / 2)
+            .attr("font-size", font_size)
+            .text(function(d) { return d[2]; });
+    }
 }
 
 $(document).ready(function(){
     $(".preview").each(function(){
         graphId = $(this).find($(".graph")).attr("id");
-        graph(graphId);
+        graph(graphId, "preview");
+        });
+
+    $("#reference").each(function(){
+        graphId = $(this).attr("id");
+        graph(graphId, "reference");
         });
 });
 
 $(window).on('resize', function (){ 
     $(".graph").contents().remove();
+
     $(".preview").each(function(){
         graphId = $(this).find($(".graph")).attr("id");
-        graph(graphId);
+        graph(graphId, "preview");
         });
-    // $(".spectrumMap").each(function(){
-    //     $(this).width($(this).parent().width());
-    //     });
+
+    $("#reference").each(function(){
+        graphId = $(this).attr("id");
+        graph(graphId, "reference");
+        });
 });
