a46664c2a6a9d49a3bf87baaf627fa5e140742f3
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-04-13 20:24:16 -0700

    #120, #121. Fix every path row listed bug. Add debounce. Figure out no scene result. Comments.
    Implement debounce to prevent ajax calls from running into each other and reduce load on database. Check for zer length return of lat/lng -> path/row query, resulting in the return of all scenes from AWS public data set query.

--- app/app/static/js/map.js
+++ app/app/static/js/map.js
@@ -1,4 +1,3 @@
-require('mapbox.js');
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
  
  
@@ -16,16 +15,14 @@ $(document).ready(function(){
     }); 
 });
 
-// Once a user finishes moving the map, send an AJAX request to Pyramid
-// which will repopulate the HTML with an updated list of the Landsat
-// scenes present.
-map.on('moveend', function() {
- 
+//  Implement debouce to prevent excessive calls to database and ajax calls
+// running into each other causing the application to hang
+var sceneList = _.debounce(function() {
     // Define the center of the map.
     var center = map.getCenter(),
         lat = center.lat,
         lng = center.lng;
- 
+    
     // Submit a post request with the relevant information.
     $.ajax({
         url: "/scene_options_ajax",
@@ -44,11 +41,11 @@ map.on('moveend', function() {
                 var num = i;
                 var n = num.toString();
                 var id = 'tab'.concat(n);
- 
+    
                 $('#pathrowgrouping').append(
                     $('<table></table>').attr('id', id)
                 );
- 
+    
                 var scenes_path_row = scenes_pr[i];
                 var newid = '#'.concat(id);
 
@@ -72,4 +69,9 @@ map.on('moveend', function() {
                 }
         }
     });
-});
+}, 1000);
+
+// Once a user finishes moving the map, send an AJAX request to Pyramid
+// which will repopulate the HTML with an updated list of the Landsat
+// scenes present.
+map.on('moveend', sceneList);
