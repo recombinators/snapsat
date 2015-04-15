9d1a9f4d251bf5bf7940bcf3361f10da375793f7
jacquestardie <hi@jacquestardie.org>
2015-04-15 14:37:11 -0700

    fix gulp

--- app/app/static/bundle.js
+++ app/app/static/bundle.js
@@ -21617,7 +21617,7 @@ module.exports={
   },
   "name": "mapbox.js",
   "description": "mapbox javascript api",
-  "version": "2.1.7",
+  "version": "2.1.6",
   "homepage": "http://mapbox.com/",
   "repository": {
     "type": "git",
@@ -21652,18 +21652,16 @@ module.exports={
   "engines": {
     "node": "*"
   },
-  "gitHead": "de0a8a3527023dee57d19efe32887d50a1414f8c",
   "bugs": {
     "url": "https://github.com/mapbox/mapbox.js/issues"
   },
-  "_id": "mapbox.js@2.1.7",
-  "_shasum": "7b2a819bd09bedefd992f262e150de60e139512b",
+  "_id": "mapbox.js@2.1.6",
+  "_shasum": "db39a14b3135a37633d756b985e6f2693c794232",
   "_from": "mapbox.js@>=2.1.6 <3.0.0",
-  "_npmVersion": "2.1.3",
-  "_nodeVersion": "0.10.32",
+  "_npmVersion": "1.4.9",
   "_npmUser": {
-    "name": "jfirebaugh",
-    "email": "john.firebaugh@gmail.com"
+    "name": "yhahn",
+    "email": "young@mapbox.com"
   },
   "maintainers": [
     {
@@ -21796,11 +21794,12 @@ module.exports={
     }
   ],
   "dist": {
-    "shasum": "7b2a819bd09bedefd992f262e150de60e139512b",
-    "tarball": "http://registry.npmjs.org/mapbox.js/-/mapbox.js-2.1.7.tgz"
+    "shasum": "db39a14b3135a37633d756b985e6f2693c794232",
+    "tarball": "http://registry.npmjs.org/mapbox.js/-/mapbox.js-2.1.6.tgz"
   },
   "directories": {},
-  "_resolved": "https://registry.npmjs.org/mapbox.js/-/mapbox.js-2.1.7.tgz"
+  "_resolved": "https://registry.npmjs.org/mapbox.js/-/mapbox.js-2.1.6.tgz",
+  "readme": "ERROR: No README data found!"
 }
 
 },{}],12:[function(require,module,exports){
@@ -22934,8 +22933,7 @@ var LMap = L.Map.extend({
         legendControl: {},
         gridControl: {},
         infoControl: false,
-        shareControl: false,
-        sanitizer: require('sanitize-caja')
+        shareControl: false
     },
 
     _tilejson: {},
@@ -23051,7 +23049,7 @@ var LMap = L.Map.extend({
         }
 
         if (this.infoControl && json.attribution) {
-            this.infoControl.addInfo(this.options.sanitizer(json.attribution));
+            this.infoControl.addInfo(json.attribution);
             this._updateMapFeedbackLink();
         }
 
@@ -23141,7 +23139,7 @@ module.exports.map = function(element, _, options) {
     return new LMap(element, _, options);
 };
 
-},{"./feature_layer":13,"./feedback":14,"./grid_control":18,"./grid_layer":19,"./info_control":21,"./legend_control":23,"./load_tilejson":24,"./mapbox_logo":27,"./share_control":30,"./tile_layer":32,"./util":34,"sanitize-caja":9}],26:[function(require,module,exports){
+},{"./feature_layer":13,"./feedback":14,"./grid_control":18,"./grid_layer":19,"./info_control":21,"./legend_control":23,"./load_tilejson":24,"./mapbox_logo":27,"./share_control":30,"./tile_layer":32,"./util":34}],26:[function(require,module,exports){
 'use strict';
 
 var geocoderControl = require('./geocoder_control'),
@@ -23486,10 +23484,6 @@ var formatPattern = /\.((?:png|jpg)\d*)(?=$|\?)/;
 var TileLayer = L.TileLayer.extend({
     includes: [require('./load_tilejson')],
 
-    options: {
-        sanitizer: require('sanitize-caja')
-    },
-
     // http://mapbox.com/developers/api/#image_quality
     formats: [
         'png', 'jpg',
@@ -23530,7 +23524,7 @@ var TileLayer = L.TileLayer.extend({
 
         L.extend(this.options, {
             tiles: json.tiles,
-            attribution: this.options.sanitizer(json.attribution),
+            attribution: json.attribution,
             minZoom: json.minzoom || 0,
             maxZoom: json.maxzoom || 18,
             tms: json.scheme === 'tms',
@@ -23577,7 +23571,7 @@ module.exports.tileLayer = function(_, options) {
     return new TileLayer(_, options);
 };
 
-},{"./load_tilejson":24,"./util":34,"sanitize-caja":9}],33:[function(require,module,exports){
+},{"./load_tilejson":24,"./util":34}],33:[function(require,module,exports){
 'use strict';
 
 var config = require('./config'),
