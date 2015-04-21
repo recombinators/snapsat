3987c4d4b44aadc6af941854a90e9fc96124df38
Mark Saiget <saiget@gmail.com>
2015-04-21 12:12:01 -0700

    #172 Get lat/lng from dom and save middle in session storage.

--- app/app/static/js/map_location.js
+++ app/app/static/js/map_location.js
@@ -1,9 +1,11 @@
 document.getElementById("js-map_location").onclick = function() {
     if (Modernizer.sessionstorage) {
         // If session storage is available, save center of path/row
-
+        var lat_lng = document.getElementsByTagName('td')[3].innerHTML.replace(/\s+/g, '').split(/\/|,/)
+        var lat = (parseFloat(lat_lng[0])+parseFloat(lat_lng[2]))/2
+        var lng = (parseFloat(lat_lng[1])+parseFloat(lat_lng[3]))/2
         sessionStorage['lat'] = lat;
-        sessionStoarge['lng'] = lng;
+        sessionStorage['lng'] = lng;
     }
     location.href = "/#create";
 }
