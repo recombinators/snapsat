3722e78f4ffe9d26970f7a9931bdbfb6249397d0
Mark Saiget <saiget@gmail.com>
2015-04-21 13:36:33 -0700

    Correct typo in map_location.js

--- app/app/static/js/map_location.js
+++ app/app/static/js/map_location.js
@@ -1,5 +1,5 @@
 document.getElementById("js-map_location").onclick = function() {
-    if (Modernizer.sessionstorage) {
+    if (Modernizr.sessionstorage) {
         // If session storage is available, save center of path/row
         var lat_lng = document.getElementsByTagName('td')[3].innerHTML.replace(/\s+/g, '').split(/\/|,/)
         var lat = (parseFloat(lat_lng[0])+parseFloat(lat_lng[2]))/2
