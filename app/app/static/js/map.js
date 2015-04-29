37e0043a84e1930d2180508d4ea3949a4993590a
Jacques Tardie <hi@jacquestardie.org>
2015-04-28 18:13:37 -0700

    geolocate users location and update basemap accordingly

--- app/app/static/js/map.js
+++ app/app/static/js/map.js
@@ -1,36 +1,43 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiamFjcXVlcyIsImEiOiJuRm9TWGYwIn0.ndryRT8IT0U94pHV6o0yng';
 
-// Create a basemap
-var map = L.mapbox.map('map', 'jacques.k7coee6a', {
-    zoomControl: true,
-    maxZoom: 7,
-    minZoom: 3
-});
-
-map.scrollWheelZoom.disable();
-map.addControl(L.mapbox.geocoderControl('mapbox.places'));
-
 
 //  Set column widths on column titles tables when page is ready
 $(document).ready(function(){
-    $.each($('.js-column_titles th'), function(i, value){
-        var wid = $($(".js-group_head th")[i]).width();
-        $(value).width(wid);
-    }); 
 
-    var lat = 47.568,
-        lng = -122.582;
+  $.each($('.js-column_titles th'), function(i, value){
+    var wid = $($(".js-group_head th")[i]).width();
+    $(value).width(wid);
+  }); 
+
+  // Set default coordinates to Seattle, WA
+  var lat = 47.568;
+  var lng = -122.582;
+
+  // Attempt to locate the user based on their IP
+  $.getJSON('http://freegeoip.net/json/', function(json) {
+    if (json) {
+      lat = json.latitude;
+      lng = json.longitude;
+    }
 
     // If available, use the stored Latitudes & Longitudes
     if (Modernizr.sessionstorage) {
-        if (sessionStorage.getItem('lat') !== null) {
-            lat = sessionStorage.lat;
-        }
-        if (sessionStorage.getItem('lng') !== null) {
-            lng = sessionStorage.lng;
-        }
+      if (sessionStorage.getItem('lat') !== null) { lat = sessionStorage.lat; }
+      if (sessionStorage.getItem('lng') !== null) { lng = sessionStorage.lng; }
     }
-    map.setView([lat, lng], 7);
+
+    // Create a basemap
+    var map = L.mapbox.map('map', 'jacques.lh797p9e', {
+      zoomControl: false,
+      maxZoom: 7,
+      minZoom: 3,
+    });
+
+    map.setView([lat, lng], 7)
+    map.scrollWheelZoom.disable()
+    map.addControl(L.mapbox.geocoderControl('mapbox.places'));
+
+  });
 });
 
 
@@ -38,81 +45,81 @@ $(document).ready(function(){
 // running into each other causing the application to hang
 var sceneList = _.debounce(function() {
 
-    // Define the center of the map.
-    var center = map.getCenter(),
-        lat = center.lat,
-        lng = center.lng;
+  // Define the center of the map.
+  var center = map.getCenter(),
+    lat = center.lat,
+    lng = center.lng;
 
-    // Submit a post request with the relevant information.
-    $.ajax({
-        url: "/scene_options_ajax",
-        dataType: "json",
-        data: {'lat': lat, 'lng': lng, },
-    }).done(function(json) {
+  // Submit a post request with the relevant information.
+  $.ajax({
+    url: "/scene_options_ajax",
+    dataType: "json",
+    data: {'lat': lat, 'lng': lng, },
+  }).done(function(json) {
 
-        if (Modernizr.sessionstorage) {
-            sessionStorage.lat = lat;
-            sessionStorage.lng = lng;
-        }
+    if (Modernizr.sessionstorage) {
+      sessionStorage.lat = lat;
+      sessionStorage.lng = lng;
+    }
 
-        scenes = json.scenes;
-         
-         // Update path-row groupings of scenes on map move
-        $('#js-pathrowgrouping').html('');
+    scenes = json.scenes;
 
-        for (var i in scenes_pr) {
-            var scenes_path_row = scenes_pr[i];
+    // Update path-row groupings of scenes on map move
+    $('#js-pathrowgrouping').html('');
 
-            // Set id tag for each new pathrowgroup.
-            var num = i;
-            var n = num.toString();
-            var id = 'pathrowgroup'.concat(n);
+    for (var i in scenes_pr) {
+      var scenes_path_row = scenes_pr[i];
 
-            $('#js-pathrowgrouping').append(
-               $('<div class="flex flex-column p2"></div>').attr('id', id)
-            );
+      // Set id tag for each new pathrowgroup.
+      var num = i;
+      var n = num.toString();
+      var id = 'pathrowgroup'.concat(n);
 
-            var newid = '#'.concat(id);
+      $('#js-pathrowgrouping').append(
+        $('<div class="flex flex-column p2"></div>').attr('id', id)
+      );
 
-            // Create new group for each path-row grouping.
-            $(newid).append(
-                "<div>" +
-                    "<div>" +
-                        "Path-Row: <span class='bold'>" + scenes_path_row[0].path + "-" + scenes_path_row[0].row + "</span> " +
-                    "</div>" +
-                    "<div>" +
-                        "Time: <span class='bold'>~" + scenes_path_row[0].average_time + "</span><span class='ml1'>UTC</span>" +
-                    "</div>" +
-                "</div>" +
-                "<div class='flex flex-justify'>" +
-                    "<div>Date</div>" +
-                    "<div class='regular gray'>Cloud Cover</div>" +
-                "</div>"
-            );
+      var newid = '#'.concat(id);
 
-            // Create new subgroup for each path-row grouping.
-            var subid = 'pathrowsubgroup'.concat(n);
-        
-            $(newid).append(
-                $('<div></div>').attr('id', subid)
-            );
+      // Create new group for each path-row grouping.
+      $(newid).append(
+        "<div>" +
+          "<div>" +
+          "Path-Row: <span class='bold'>" + scenes_path_row[0].path + "-" + scenes_path_row[0].row + "</span> " +
+          "</div>" +
+          "<div>" +
+          "Time: <span class='bold'>~" + scenes_path_row[0].average_time + "</span><span class='ml1'>UTC</span>" +
+          "</div>" +
+          "</div>" +
+          "<div class='flex flex-justify'>" +
+          "<div>Date</div>" +
+          "<div class='regular gray'>Cloud Cover</div>" +
+          "</div>"
+      );
 
-            var newsubid = '#'.concat(subid);
+      // Create new subgroup for each path-row grouping.
+      var subid = 'pathrowsubgroup'.concat(n);
+
+      $(newid).append(
+        $('<div></div>').attr('id', subid)
+      );
+
+      var newsubid = '#'.concat(subid);
 
 
-            // Generate entry for each date within a path-row group.
-            for (var k in scenes_path_row) {
-                $(newsubid).append(
-                    "<div>" +
-                        "<a style='text-decoration: none' class='flex flex-justify button-transparent' href ='/scene/" + scenes_path_row[k].entityid + "'>" +
-                            "<div class='regular black mr4'>" + scenes_path_row[k].acquisitiondate + "</div>" +
-                            "<div class='regular gray'>" + scenes_path_row[k].cloudcover + "%</div>" + 
-                        "</a>" +
-                    "</div>"
-                );
-            }
-        }
-    });
+      // Generate entry for each date within a path-row group.
+      for (var k in scenes_path_row) {
+        $(newsubid).append(
+          "<div>" +
+            "<a style='text-decoration: none' class='flex flex-justify button-transparent' href ='/scene/" + scenes_path_row[k].entityid + "'>" +
+            "<div class='regular black mr4'>" + scenes_path_row[k].acquisitiondate + "</div>" +
+            "<div class='regular gray'>" + scenes_path_row[k].cloudcover + "%</div>" + 
+            "</a>" +
+            "</div>"
+        );
+      }
+    }
+  });
 }, 250);
 
 // Once a user finishes moving the map, send an AJAX request to Pyramid
