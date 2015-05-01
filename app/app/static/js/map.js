8e03255ce2e7a4a2cf4ce214edb3318a8cf4acab
Jacques Tardie <hi@jacquestardie.org>
2015-05-01 12:25:07 -0700

    Remove geocoder

--- app/app/static/js/map.js
+++ app/app/static/js/map.js
@@ -35,9 +35,6 @@ $(document).ready(function(){
 
     map.setView([lat, lng], 7)
     map.scrollWheelZoom.disable()
-    map.addControl(L.mapbox.geocoderControl('mapbox.places', {
-      keepOpen: true
-    }));
 
   });
 });
