72bea83bae225209f507a7e5b5cff137ef342934
Jacques Tardie <hi@jacquestardie.org>
2015-05-07 15:22:01 -0700

    add about page

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -18,6 +18,7 @@ def main(global_config, **settings):
 
     # Define routes
     config.add_route('index', '/')
+    config.add_route('about', '/about')
     config.add_route('guide', '/guide')
     config.add_route('request_composite', '/request_composite/{scene_id}')
     config.add_route('request_preview', '/request_preview/{scene_id}')
